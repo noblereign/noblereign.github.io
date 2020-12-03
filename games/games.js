b096b72f00afcba2a48fe919ba0c7af5422881ef
Noble <noblereign@users.noreply.github.com>
2020-12-02 19:15:29 -0800

    omg i hate java script

--- games/games.js
+++ games/games.js
@@ -5,27 +5,26 @@ function loadGames() {
 		if (this.readyState == 4 && this.status == 200) {
 			const fileList = this.responseText.split('\n'); // Split by lines
 			let currentFolder = '';
-			
+
 			const filePaths = fileList
-			.map(f => { // Build correct path for each file
-				let filePath = '';
-				let artistName = '';
-				
-				if (f) {
-					if (f[0] === '.') {
-						currentFolder = f.replace('.', '').replace(':', '/');
+				.map(f => { // Build correct path for each file
+					let filePath = '';
+					let artistName = '';
+
+					if (f) {
+						if (f[0] === '.') {
+							currentFolder = f.replace('.', '').replace(':', '/');
+						} else if (f[f.length - 1] !== '/') {
+							filePath = `${location.href}${currentFolder}${f}`;
+						}
 					}
-					else if (f[f.length - 1] !== '/') {
-						filePath = `${location.href}${currentFolder}${f}`;
-					}
-				}
-	  
-				return filePath;
-			})
-			.filter(f => f); // Remove empty lines
-	  
+
+					return filePath;
+				})
+				.filter(f => f); // Remove empty lines
+
 			const imagesContainer = document.getElementById('grid-container');
-	  
+
 			filePaths.map(f => { // Create and put images to the DOM
 				let artistName = f.replace('https://www.glacier.dog//games/cards/', '').split('/')[0];
 				const cardBase = document.createElement('div');
@@ -40,42 +39,42 @@ function loadGames() {
 				content.appendChild(title);
 				const description = document.createElement('p');
 				content.appendChild(description);
-				
+
 				const playButton = document.createElement('button');
 				playButton.setAttribute('id', "play");
-				
+
 				const playIcon = document.createElement('i');
 				playIcon.setAttribute('class', "fas fa-play");
 				playButton.appendChild(playIcon);
-				
+
 				content.appendChild(playButton);
 				const moreButton = document.createElement('button');
 				moreButton.setAttribute('id', "more");
 				moreButton.innerHTML = "View on Roblox";
 				content.appendChild(moreButton);
-				
-				$.get(f, function(myContentFile) {
-				   var lines = myContentFile.split("\r\n");
 
-				   for(var i  in lines){
-					   if (i == 0){ //ID
-							backgroundImg.style.["background-image"] = "https://www.roblox.com/asset-thumbnail/image?assetId=" + lines[i] + "&width=768&height=432&format=png";
+				$.get(f, function(myContentFile) {
+					var lines = myContentFile.split("\r\n");
+
+					for (var i in lines) {
+						if (i === 0) { //ID
+							backgroundImg.style.backgroundImage = "https://www.roblox.com/asset-thumbnail/image?assetId=" + lines[i] + "&width=768&height=432&format=png";
 							playButton.setAttribute('onclick', "location.href='roblox://" + lines[i] + "'");
 							moreButton.setAttribute('onclick', "location.href='rblx.games/" + lines[i] + "'");
-					   } else if (i == 1) { //Title
+						} else if (i === 1) { //Title
 							title.innerHTML = lines[i];
-					   } else{ //Description
+						} else { //Description
 							content.innerHTML = lines[i];
-					   }
-					  //here your code
-					  //each line is "lines[i]"
+						}
+						//here your code
+						//each line is "lines[i]"
 
-					  //save in object "myObject": 
-					  //print in console
-					  console.log("line " + i + " :" + lines[i]);
-				   }
+						//save in object "myObject": 
+						//print in console
+						console.log("line " + i + " :" + lines[i]);
+					}
 				}, 'text');
-				
+
 				imagesContainer.appendChild(cardBase);
 			});
 		}
